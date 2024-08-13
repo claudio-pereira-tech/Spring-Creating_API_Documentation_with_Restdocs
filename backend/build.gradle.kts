365a1569dbaf9631afd0686e554ee1ee5b619ea6
Cláudio de Oliveira Pereira <claudio@pereira.tech>
2024-08-12 20:53:09 -0700

    Add Spring REST Docs dependency

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("org.asciidoctor.jvm.convert") version "4.0.3"
 }
 
 group = "tech.pereira"
@@ -17,13 +18,25 @@ repositories {
 	mavenCentral()
 }
 
+extra["snippetsDir"] = file("build/generated-snippets")
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web:3.3.2")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.2")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc:3.0.1")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.3")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.test {
+	outputs.dir(project.extra["snippetsDir"]!!)
+}
+
+tasks.asciidoctor {
+	inputs.dir(project.extra["snippetsDir"]!!)
+	dependsOn(tasks.test)
 }
