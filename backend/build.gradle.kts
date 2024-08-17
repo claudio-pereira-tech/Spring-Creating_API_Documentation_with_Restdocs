ab7103279a05fc6a851fad81ba32e65bcacea21d
Cláudio de Oliveira Pereira <claudio@pereira.tech>
2024-08-16 20:53:21 -0700

    Add GraalVM Native Support

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("org.graalvm.buildtools.native") version "0.10.2"
 	id("org.asciidoctor.jvm.convert") version "4.0.3"
 }
 
