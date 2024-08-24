fac5c91d69e9a4f1d24758de0d8678a4a712c8b1
Cláudio de Oliveira Pereira <claudio@pereira.tech>
2024-08-23 21:07:52 -0700

    Fix endpoint URI

--- backend/src/test/java/tech/pereira/springrestdocs/WebLayerTest.java
+++ backend/src/test/java/tech/pereira/springrestdocs/WebLayerTest.java
@@ -1,14 +1,20 @@
 package tech.pereira.springrestdocs;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import tech.pereira.springrestdocs.controllers.HomeController;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -16,12 +22,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @WebMvcTest(HomeController.class)
-@AutoConfigureRestDocs
 public class WebLayerTest {
 
-    @Autowired
     private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentationContext) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentationContext).uris()
+                        .withScheme("http")
+                        .withHost("localhost/api")
+                        .withPort(80))
+                .build();
+    }
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
